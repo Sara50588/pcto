3960490bdf8bf5fa8855892912c3b7699c797ee8
Sara50588 <156684207+Sara50588@users.noreply.github.com>
2024-01-16 16:52:07 +0100

    Add files via upload

---
+++ culo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int main(){
+    float a;
+    float b;
+    float c;
+    printf("inserisce le lunghezze dei lati\n");
+    scanf("%f %f %f" ,&a ,&b,&c);
+    if(a==b){
+        if (a!=c){
+            printf("il t e isocelo")
+        }else if (a=c){
+            printf("il triangolo è equi\n");
+        }else if (b!=c){      
+            printf("il tr isoa\n");
+            }}
+            if (a!=b){
+        else if (b!=c){
+            printf("porcodiooo scaleno\n");
+
+        }else(b==c){
+            ("dioporco isoscel");
+             }}
+             }
