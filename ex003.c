1e635a52a6ae27d119dc2f8cab3f98fdf11f72f6
Sara50588 <156684207+Sara50588@users.noreply.github.com>
2024-01-16 09:52:05 +0100

    Add files via upload

---
+++ ex003.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    int numero;
+
+    printf("quanti anni hai?:\n");
+    scanf("%d",&numero);
+    printf("cazzo dimostri meno di : %d\n",numero);
+    return(0);
+}
