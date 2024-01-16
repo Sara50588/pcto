b5438285489ce4b11ff48bc9e5deefefd13a3aa5
Sara50588 <156684207+Sara50588@users.noreply.github.com>
2024-01-16 17:05:05 +0100

    Add files via upload

--- culo.c
+++ culo.c
@@ -7,17 +7,13 @@
     scanf("%f %f %f" ,&a ,&b,&c);
     if(a==b){
         if (a!=c){
-            printf("il t e isocelo")
-        }else if (a=c){
+            printf("i porcodioooooooooooe isocelo\n");
+        }else if (a==c){
             printf("il triangolo è equi\n");
-        }else if (b!=c){      
-            printf("il tr isoa\n");
-            }}
-            if (a!=b){
-        else if (b!=c){
-            printf("porcodiooo scaleno\n");
-
-        }else(b==c){
-            ("dioporco isoscel");
+        }} else if(a!=b){
+         if (b!=c){
+            printf("porcodiooo di scaleno\n");
+             }else if(b==c){
+           printf ("dioporco isoscel\n");
              }}
              }
