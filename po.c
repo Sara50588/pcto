cb828b5024959cf4a1ef3595700abff74a030c80
Sara50588 <156684207+Sara50588@users.noreply.github.com>
2024-01-17 17:05:02 +0100

    Add files via upload

---
+++ po.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+    float a;
+    float m;
+    int i;
+    i=0;
+    printf("inserisci un numero\n");
+     scanf("%f",&a);
+    while(a>=0){
+    i++;
+    a=m+a;
+    printf("media:%f\n", a/i);
+    printf("inserisci un numero\n");
+     scanf("%f",&a);
+   
+    
+
+    }
+   
+
+      }
