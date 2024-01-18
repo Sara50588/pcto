c3882afae7160bde5a5da1f0d6a4536521160dd6
Sara50588 <156684207+Sara50588@users.noreply.github.com>
2024-01-18 09:33:00 +0100

    Add files via upload

--- po.c
+++ po.c
@@ -7,16 +7,11 @@ int main (){
     i=0;
     printf("inserisci un numero\n");
      scanf("%f",&a);
-    while(a>=0){
-    i++;
-    a=m+a;
-    printf("media:%f\n", a/i);
-    printf("inserisci un numero\n");
-     scanf("%f",&a);
-   
-    
-
-    }
-   
-
+    while(a>=0 && m>=0){
+      i++;
+      a=m+a;
+      printf("media:%f\n", a/i);
+      printf("inserisci un numero\n");
+      scanf("%f",&m);
       }
+}
